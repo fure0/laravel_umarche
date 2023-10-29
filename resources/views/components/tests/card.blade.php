66e591b10737904f74a58ea5fa39167945caa4c8
KimTaeyoun <kimty1121@gmail.com>
2023-10-29 23:47:50 +0900

    29 classBase

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -5,7 +5,7 @@
 ])
 
 <div {{ $attributes->merge([
-    'class' => 'border-2 shadow-md w-1n4 p-2'
+    'class' => 'border-2 shadow-md w-1/4 p-2'
     ]) }}>
     <div>{{ $title }}</div>
     <div>画像</div>
